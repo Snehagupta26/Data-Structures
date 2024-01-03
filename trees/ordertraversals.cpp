d9aef3b8396de557fc297f6aa0056eb80e193179
Snehagupta26 <02snehagupta@gmail.com>
2024-01-03 19:39:18 +0530

    hg

---
+++ trees/ordertraversals.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+void preorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data;
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data;
+   
+}
+void inorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data;
+    inorder(root->right);
+}
+int main(){
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    cout<<"preorder traversal is : "<<endl;
+    preorder(root);
+    cout<<endl;
+    cout<<"inorder traversal is : "<<endl;
+    inorder(root);
+    cout<<endl;
+    cout<<"postorder traversal is : "<<endl;
+    postorder(root);
+}
